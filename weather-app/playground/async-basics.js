e4291f2bda23e0f09d33207b0726945c640c508b
ZARMACARON\a-soleymani <Solimaniali90@gmail.com>
2022-11-07 11:09:57 +0330

    async سنکرون آسنکرون

---
+++ weather-app/playground/async-basics.js
@@ -0,0 +1,17 @@
+/// سنکرون -آسنکرون
+
+// setTimeout(() => {
+//     console.log('After two second')
+// }, 2000) //2000 ms=2s
+
+
+console.log('Strating app');
+
+setTimeout(() => {
+    console.log('Use setTimeout function')
+}, 2000)
+
+setTimeout(() => {
+    console.log('Use  zero setTimeout')
+},0)
+console.log('Finishing app');
