f3d2e443d0c7b0d398ff9e7ab1a7245eab1cd896
ali solimani <solimaniali90@gmail.com>
2023-01-18 00:05:53 +0330

    add hbs to project

--- node-web-server/Server.js
+++ node-web-server/Server.js
@@ -1,15 +1,47 @@
 const express=require('express');
+const hbs=require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/Partials')
+
+app.use(express.static(__dirname + '/public'))
+app.set('view engie','hbs');
+
+
+
+
+
 //GeT POST PUT PATCH DELETE -------------->Http Request
 //http://www.google.ir/vuejs --->Get Request
+// app.get('/',(req,res)=>{
+//   res.send("<h1>hello express</h1>")
+// })
+
+// app.get('/',(req,res)=>{
+//   res.send({
+//     name:'hello',
+//     website:[
+//       'test1',
+//       'test2'
+//     ]
+//   })
+// })
+
+
 app.get('/',(req,res)=>{
-  res.send("<h1>hello express</h1>")
+  res.render('home.hbs',{
+    pageTitle:'صفحه اصلی',
+    currentYear:new Date().getFullYear()
+  })
 })
-
-
 app.get('/about',(req,res)=>{
-    res.send("<h1>About Page</h1>")
+    res.render('about.hbs', {
+      pageTitle:'درباره ما 2',
+      currentYear:new Date().getFullYear()
+    })
 })
-app.listen(3000);
+app.listen(3000, () =>{
+
+  console.log('Server ru o 300 Port')
+});
